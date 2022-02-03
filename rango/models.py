d2123200402a03f98419f92280608cc35524fd20
Yipeng Xiong <2612713x@student.gla.ac.uk>
2022-02-03 23:16:42 +0000

    Chapter 5: debug

--- rango/models.py
+++ rango/models.py
@@ -4,6 +4,8 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -13,7 +15,7 @@ class Category(models.Model):
 
 
 class Page(models.Model):
-    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
