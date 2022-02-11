48e348c071c9fa1da23e50ae52ad0969f62c9dba
Yipeng Xiong <2612713x@student.gla.ac.uk>
2022-02-11 06:36:10 +0000

    Chapter 9: Implement the register page and register a new user, successfully checked it in the admin page.

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,9 @@
 from django import forms
+from django.contrib.auth.models import User
+
 from rango.models import Page, Category
+from rango.models import UserProfile
+
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128, help_text="Please enter the category name.")
@@ -12,6 +16,7 @@ class CategoryForm(forms.ModelForm):
         # Provide an association between the ModelForm and a model
         model = Category
         fields = ('name',)
+
 
 class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
@@ -41,4 +46,18 @@ class PageForm(forms.ModelForm):
             url = f'http://{url}'
             cleaned_data['url'] = url
         return cleaned_data
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',)
 
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -105,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-        'OPTIONS': {'min+length': 6, }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
